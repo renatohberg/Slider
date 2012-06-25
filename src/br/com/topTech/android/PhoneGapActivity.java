6f0a7c43ca5ee5412b790affe22826d826eda334
RenatoHBerg <renatohberg@gmail.com>
2012-06-24 22:29:12 -0300

    Signed-off-by: RenatoHBerg <renatohberg@gmail.com>

---
+++ src/br/com/topTech/android/PhoneGapActivity.java
@@ -0,0 +1,14 @@
+package br.com.topTech.android;
+
+import org.apache.cordova.DroidGap;
+
+import android.os.Bundle;
+
+public class PhoneGapActivity extends DroidGap {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/index.html");
+    }
+}
